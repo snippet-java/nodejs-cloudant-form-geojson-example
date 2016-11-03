f1e65e0678910b2da573157e6419094fdf84e1f8
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-11-04 01:41:54 +0800

    multiple changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,8 @@
+var map;
+
 $(function() {
 	loadList(true);
+	loadMap();
 	$("#submit").click(addItem);
 	$("#list").on("click", "li .glyphicon-trash", deleteItem);
 	$("#list").on("click", "li .id", populateForm);
@@ -25,6 +28,7 @@ function loadList(first) {
 	})
 }
 
+
 function createListItem(id, name, description, point) {
 	var $target = $("#list li[doc_id='"+id+"']");
 	if ($target.length > 0) {
@@ -32,7 +36,7 @@ function createListItem(id, name, description, point) {
 		$target.find(".name").text(name);
 		$target.find(".description").text(description);
 		if (typeof point === "object") {
-			point = point.coordinates[0] + "," + point.coordinates[1]
+			point = parseFloat(point.coordinates[0]).toFixed(4) + "," + parseFloat(point.coordinates[1]).toFixed(4);
 		}
 		$target.find(".point").text(point);
 		return 0;
@@ -75,23 +79,90 @@ function addItem() {
 	$.get("/cloudant/update?id=" + id + "&name=" + name + "&description=" + description + "&point=" + point)
 	.done(function(data) {
 		var result = createListItem(id, name, description, point);
-		if (result)
+		if (result) {
 			alert("item " + id + " added!");
-		else
+			addFeature(id, name, point);
+		} else {
 			alert("item " + id + " updated!");
+			updateFeature(id, name, point);
+		}
+		
+		$("#_point").val("");
+		$("#_id").val("");
+		$("#_name").val("");
+		$("#_description").val("");
 	})
 }
 
-
 function deleteItem() {
 	var id = $(this).parents(".list-group-item").attr("doc_id");
-	console.log(id);
+
 	$.get("/cloudant/destroy?id=" + id)
 	.done(function(data) {
 		var $target = $("#list li[doc_id='"+id+"']");
 		$target.hide('slow', function(){ 
 			$target.remove();
+			removeFeature(id)
 			alert("item " + id + " removed!");
 		});
 	})
+}
+
+//== Map functions =======================================================//
+
+function loadMap() {
+	var mapOptions = {
+			center: new google.maps.LatLng(0,0),
+			zoom: 5
+	};
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	google.maps.event.addListener(map.data, 'addfeature', focusMap);
+	google.maps.event.addListener(map.data, 'removefeature', focusMap);
+	map.data.loadGeoJson('cloudant/geojson', focusMap);
+	map.data.setStyle(function(feature) {
+		if (feature.getProperty('title'))
+			return {title: feature.getProperty('title')};
+		return {};
+	});
+}
+
+function focusMap() {
+	console.log("Focussing map");
+	map.setCenter({lat:0,lng:0});
+	var bounds = new google.maps.LatLngBounds();
+	map.data.forEach(function(feature) {
+		feature.getGeometry().forEachLatLng(function(latlng) {
+			bounds.extend(latlng);
+			map.fitBounds(bounds);			
+		})
+	});
+}
+
+function addFeature(id, name, point) {
+	var featureOptions = {
+			id : id,
+			properties : {
+				updated : new Date().getTime(),
+				title : name
+			},
+			geometry : {lat: parseFloat(point.split(",")[1]), lng: parseFloat(point.split(",")[0])}
+	};
+	map.data.add(featureOptions);
+}
+
+function updateFeature(id, name, point) {
+	var feature = map.data.getFeatureById(id)
+	if (feature) {
+		feature.setGeometry({lat: parseFloat(point.split(",")[1]), lng: parseFloat(point.split(",")[0])});
+		feature.setProperty("updated", new Date().getTime());
+		feature.setProperty("title", name);
+	} else {
+		addFeature(id, name, point);
+	}
+}
+
+function removeFeature(id) {
+	var feature = map.data.getFeatureById(id);
+	if (feature)
+		map.data.remove(feature);
 }
